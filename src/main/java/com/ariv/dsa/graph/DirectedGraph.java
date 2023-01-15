4b1c15cc0dbf3dbc97df94da76fcf65c293ec880
Ariv <arivbits@gmail.com>
2023-01-15 21:21:33 +0530

    Directed Graph

--- src/main/java/com/ariv/dsa/graph/DirectedGraph.java
+++ src/main/java/com/ariv/dsa/graph/DirectedGraph.java
@@ -1,5 +1,20 @@
 package com.ariv.dsa.graph;
 
+import java.util.LinkedList;
+
 public class DirectedGraph {
-	
+
+	int V;
+	LinkedList<Integer>[] adj;
+
+	public DirectedGraph() {
+		this.V = V;
+		adj = new LinkedList[V];
+		for (int i = 0; i < V; i++)
+			adj[i] = new LinkedList<Integer>();
+	}
+
+	void addEdge(int v, int w) {
+		adj[v].add(w);
+	}
 }
