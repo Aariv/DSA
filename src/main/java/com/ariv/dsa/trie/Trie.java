d5a3c6e2191164f1c6141b0fd19ecf99c619f4ca
Ariv <arivbits@gmail.com>
2023-01-16 21:49:54 +0530

    Trie DS Implementation

--- src/main/java/com/ariv/dsa/trie/Trie.java
+++ src/main/java/com/ariv/dsa/trie/Trie.java
@@ -1,5 +1,53 @@
 package com.ariv.dsa.trie;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Trie Java Implementation 
+ */
 public class Trie {
 
+	private class TrieNode {
+		Map<Character, TrieNode> children;
+		boolean endOfWord;
+
+		public TrieNode() {
+			children = new HashMap<Character, TrieNode>();
+			endOfWord = false;
+		}
+	}
+
+	private final TrieNode root;
+
+	public Trie() {
+		root = new TrieNode();
+	}
+
+	public void insert(String word) {
+		TrieNode current = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			TrieNode node = current.children.get(ch);
+			if (node == null) {
+				node = new TrieNode();
+				current.children.put(ch, node);
+			}
+			current = node;
+		}
+		current.endOfWord = true;
+	}
+
+	public boolean search(String word) {
+		TrieNode current = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			TrieNode node = current.children.get(ch);
+			if (node == null) {
+				return false;
+			}
+			current = node;
+		}
+		return current.endOfWord;
+	}
 }
