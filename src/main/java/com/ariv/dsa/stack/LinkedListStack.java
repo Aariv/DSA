487e1dfb9fd3378bb1a80fe8b33b61bcbb3ae956
Ariv <arivbits@gmail.com>
2023-01-16 21:54:20 +0530

    Stack Using LinkedList

--- src/main/java/com/ariv/dsa/stack/LinkedListStack.java
+++ src/main/java/com/ariv/dsa/stack/LinkedListStack.java
@@ -2,4 +2,39 @@ package com.ariv.dsa.stack;
 
 public class LinkedListStack {
 
+	private class Node {
+		int data;
+		Node next;
+	}
+
+	private Node top;
+
+	public boolean isEmpty() {
+		return top == null;
+	}
+
+	public void push(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		newNode.next = top;
+		top = newNode;
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			System.out.println("Stack is empty.");
+			return Integer.MIN_VALUE;
+		}
+		int data = top.data;
+		top = top.next;
+		return data;
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			System.out.println("Stack is empty.");
+			return Integer.MIN_VALUE;
+		}
+		return top.data;
+	}
 }
