29ef5c993ec466c3741a9fd581e38c8bb93a6dc8
Ariv <arivbits@gmail.com>
2023-01-24 22:02:50 +0530

    Implemented Queue>

--- src/main/java/com/ariv/dsa/queue/StackQueue.java
+++ src/main/java/com/ariv/dsa/queue/StackQueue.java
@@ -1,5 +1,40 @@
 package com.ariv.dsa.queue;
 
+import java.util.Stack;
+
 public class StackQueue {
 
+	private Stack<Integer> inputStack;
+	private Stack<Integer> outputStack;
+
+	public StackQueue() {
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
+
+	public void add(int value) {
+		inputStack.push(value);
+	}
+
+	public int remove() {
+		if (outputStack.isEmpty()) {
+			while (!inputStack.isEmpty()) {
+				outputStack.push(inputStack.pop());
+			}
+		}
+		return outputStack.pop();
+	}
+
+	public int peek() {
+		if (outputStack.isEmpty()) {
+			while (!inputStack.isEmpty()) {
+				outputStack.push(inputStack.pop());
+			}
+		}
+		return outputStack.peek();
+	}
+
+	public boolean isEmpty() {
+		return inputStack.isEmpty() && outputStack.isEmpty();
+	}
 }
